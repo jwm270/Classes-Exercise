97fc8f12ffc8795b0fd0953ee10c60efd210f191
Jimmy Moore <jwm270@gmail.com>
2025-06-25 13:32:42 -0500

    exercise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var carOne  =   new Car();
+            carOne.Make = "BMW";
+            carOne.Model = "Slow";
+            carOne.Year = 2000;
+
+            Console.WriteLine($"This is a {carOne.Make} and {carOne.Model} made in the year {carOne.Year}.");
         }
     }
 }
